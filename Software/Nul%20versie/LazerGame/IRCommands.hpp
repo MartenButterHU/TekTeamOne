ee9a226fd817d14980cebba7f66c08c1eb534f14
Arco Gelderblom <arco.gelderblom@ziggo.nl>
2016-10-06 12:55:38 +0200

    Revert "Add files via upload"
    This reverts commit 5665809d895a6cce8833146dd10782c8c0cc1d97.

--- Software/Nul%20versie/LazerGame/IRCommands.hpp
+++
@@ -1,8 +0,0 @@
-
-
-class IRCommands{
-public:
-	static const int test			=	0b101010101010101;
-	static const int testOn			=	0b111111111111111;
-	static const int testOff		=	0b000000000000000;
-};
--- Software/Nul%20versie/LazerGame/IRSendController.hpp
+++
@@ -1,7 +0,0 @@
-class IRSendController{
-private:
-	
-public:
-	
-	
-};
